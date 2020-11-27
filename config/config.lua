bbcd969ff1fb8dc4be1380794999dd12f72a29c3
Will Rogers <wjrogers@gmail.com>
2020-11-26 21:29:22 -0500

    Disable nameplates, party, and raid

--- config/config.lua
+++ config/config.lua
@@ -7,3 +7,10 @@ local cfg, m, filters, watchers = ns.cfg, ns.m, ns.filters, ns.watchers
 -- ------------------------------------------------------------------------
 
 -- Important: Override each property individually or copy all the defaults
+
+
+-- disable units we use other mods for
+cfg.units.nameplate.show = false
+cfg.units.party.show = false
+cfg.units.raid.show = false
+
